f78a0176f9e55a4e0cddaf6758dbe20b9d22504d
Janet Scanlon <janet.Lscanlon@gmail.com>
2024-02-26 23:46:45 -0600

    pet status problems lol

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -37,7 +37,7 @@ console.log(food);
 
 // 8 - Create a variable called `pets` and set it to the value of the number of pets you have
 //     Console log the value of `pets`
-let pets = 2
+let pets = 8;
 console.log(pets);
 
 // 9 - Create a variable called `friendsPets` and assign it the value of the number of pets your friend has
@@ -47,7 +47,7 @@ console.log(friendsPets);
 pets += 2; 
 console.log(pets);
 // 11 - Create a constant variable called `allowedPets` and set it to a number value of your choice
-const allowedPets = 5;
+const allowedPets = 8;
 console.log(allowedPets);
 // 12 - Create a variable called `result`. Create a conditional:
 //      if adventurous is true, set `result` to be "Adventures are great!", 
@@ -85,6 +85,17 @@ else {
 //      set `petStatus` to the value of "I have enough pets" 
 //      if the value of `pets` is greater than the value of `allowedPets`.
 //      set `petStatus` to the value of "Oh no, I have too many pets!"
+let petStatus; 
+if( pets == allowedPets ){
+  petStatus = 'I have enough pets';
+}
+else if(pets > allowedPets ){
+    petStatus = 'Oh no, I have too many pets!';
+  }
+  else if(pets < allowedPets ){
+        petStatus = 'I can have more pets';
+    }
+console.log(petStatus);
 
 // STRETCH GOALS:
 
